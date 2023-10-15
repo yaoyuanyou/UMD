f2db2e045436b91bacf792fc5e177f8d8be98041
Tony Yao <72855371+yuanyouyao@users.noreply.github.com>
2023-10-15 01:58:46 -0400

    Delete INST630_Programming_For_The_Information_Professional directory

--- INST630_Programming_For_The_Information_Professional/INST630 Programming For The Information Professional /Assignment 2/color.js
+++
@@ -1,9 +0,0 @@
-const changeButton = document.querySelector(".btn");
-
-function changeButtonColor(){
-  console.log("changeButtonColor");
-  const titleColor = document.querySelector("h1");
-  titleColor.classList.toggle("newcolor");
-}
-
-changeButton.addEventListener("click", () => changeButtonColor());
